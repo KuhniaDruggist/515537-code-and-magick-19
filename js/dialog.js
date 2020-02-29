7964d241637541dbaa8cbc9e40c8319d5d6ea53c
Алексей Кухаренко <druggist5059@gmail.com>
2020-02-29 12:32:57 +0300

    add new module -dialog(Отвечает за открытие и закрытие попапа)

---
+++ js/dialog.js
@@ -0,0 +1,41 @@
+'use strict';
+(function () {
+
+  var userDialog = document.querySelector('.setup');
+  var userDialogOpen = document.querySelector('.setup-open');
+  var userDialogClose = userDialog.querySelector('.setup-close');
+  var userNameInput = userDialog.querySelector('.setup-user-name');
+
+  var onPopupEscPress = function (evt) {
+    if (userNameInput !== document.activeElement) {
+      window.utils.isEscEvent(evt, closePopup);
+    }
+  };
+
+  var openPopup = function () {
+    userDialog.classList.remove('hidden');
+    document.addEventListener('keydown', onPopupEscPress);
+  };
+
+  var closePopup = function () {
+    userDialog.classList.add('hidden');
+    document.removeEventListener('keydown', onPopupEscPress);
+  };
+
+  userDialogOpen.addEventListener('click', function () {
+    openPopup();
+  });
+
+  userDialogOpen.addEventListener('keydown', function (evt) {
+    window.utils.isEnterEvent(evt, openPopup);
+  });
+
+  userDialogClose.addEventListener('click', function () {
+    closePopup();
+  });
+
+  userDialogClose.addEventListener('keydown', function (evt) {
+    window.utils.isEnterEvent(evt, closePopup);
+  });
+
+})();
