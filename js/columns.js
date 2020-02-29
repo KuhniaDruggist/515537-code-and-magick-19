744aa19afd6e092f9791052a45e070e3eacad03a
Алексей Кухаренко <druggist5059@gmail.com>
2020-02-29 12:25:34 +0300

    add new module -columns(Отрисовка колонок статистики)

---
+++ js/columns.js
@@ -0,0 +1,29 @@
+'use strict';
+(function () {
+
+  var YOU_COLUMN_COLOR = 'rgba(255, 0, 0, 1)';
+  var MIN_SATURATION = 0;
+  var MAX_SATURATION = 100;
+
+  window.columns = {
+    maxHeightColumn: function (arr) {
+      var maxElement = arr[0];
+
+      for (var i = 1; i < arr.length; i++) {
+        if (arr[i] > maxElement) {
+          maxElement = arr[i];
+        }
+      }
+      return maxElement;
+    },
+    colorColumns: function (playerName) {
+      if (playerName === 'Вы') {
+        var columnColor = YOU_COLUMN_COLOR;
+      } else {
+        columnColor = 'hsl(240, ' + window.utils.randomSaturation(MIN_SATURATION, MAX_SATURATION) + '%,' + ' 50%)';
+      }
+      return columnColor;
+    }
+  };
+
+})();
