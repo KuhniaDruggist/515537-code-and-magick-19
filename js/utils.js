4ee98b7f0535806d485c0f4866ef83f1dd324396
Алексей Кухаренко <druggist5059@gmail.com>
2020-03-09 22:57:29 +0300

    Добавляет в модуль код выводимой ошибки

--- js/utils.js
+++ js/utils.js
@@ -26,11 +26,25 @@
     return Math.floor(Math.random() * (max - min + 1)) + min;
   };
 
+  var onError = function (errorMessage) {
+    var nodeError = document.createElement('div');
+    nodeError.style = 'z-index: 100; width: 400px; text-align: center; background-color: black;';
+    nodeError.style.position = 'absolute';
+    nodeError.style.top = '30%';
+    nodeError.style.left = 'calc(50% - 200px)';
+    nodeError.style.fontSize = '30px';
+    nodeError.style.color = 'orange';
+
+    nodeError.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', nodeError);
+  };
+
   window.utils = {
     isEscEvent: pressEsc,
     isEnterEvent: pressEnter,
     randomElementFromArray: getRandomElementFromArray,
-    randomSaturation: getrandomSaturation
+    randomSaturation: getrandomSaturation,
+    onError: onError
   };
 
 })();
