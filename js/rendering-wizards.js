a426a30bee79c1ebab990c68cb8eb26985016156
Алексей Кухаренко <druggist5059@gmail.com>
2020-03-09 23:03:35 +0300

    Реализует прием данных о похожих волшебниках с сервера

--- js/rendering-wizards.js
+++ js/rendering-wizards.js
@@ -8,28 +8,27 @@
   .content
   .querySelector('.setup-similar-item');
 
-  var wizards = window.wizards.createWizards();
-
   var renderWizard = function (wizard) {
     var wizardElement = similarWizardTemplate.cloneNode(true);
 
     wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
 
     return wizardElement;
   };
 
-  var renderWizards = function () {
+  var onLoad = function (wizards) {
     var fragment = document.createDocumentFragment();
-    for (var j = 0; j < wizards.length; j++) {
-      fragment.appendChild(renderWizard(wizards[j]));
+    for (var i = 0; i < 4; i++) {
+      fragment.appendChild(renderWizard(wizards[i]));
     }
-    return fragment;
+
+    similarListElement.appendChild(fragment);
+
+    userDialog.querySelector('.setup-similar').classList.remove('hidden');
   };
 
-  similarListElement.appendChild(renderWizards());
-
-  userDialog.querySelector('.setup-similar').classList.remove('hidden');
+  window.backend.getLoad(onLoad, window.utils.onError);
 
 })();
